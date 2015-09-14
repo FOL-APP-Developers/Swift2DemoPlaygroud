00e669081404794b4964606c744867ae2c7bd419
Peter Schumacher <peter.schumacher@finanzen100.de>
2015-09-14 17:49:51 +0200

    last page fetches online

--- Swift2DemoProject.playground/Pages/All Together.xcplaygroundpage/Contents.swift
+++ Swift2DemoProject.playground/Pages/All Together.xcplaygroundpage/Contents.swift
@@ -8,11 +8,18 @@ let tableViewController = PlaygroundTableViewController(style: .Plain)
 let naviController = UINavigationController(rootViewController: tableViewController)
 naviController.view.frame = CGRect(origin: CGPointZero, size: CGSize(width: 320, height: 640))
 
-getOfflineJSON { (json) -> Void in
+NetworkRequestHandler.getOverviewJSON { (json) -> Void in
     if let itemJSONArray = json["items"] as? JSONArray {
         tableViewController.modelArray = PlaygroundTeaserModel.createModelArray(itemJSONArray)
+        tableViewController.tableView.reloadData()
     }
 }
+//
+//getOfflineJSON { (json) -> Void in
+//    if let itemJSONArray = json["items"] as? JSONArray {
+//        tableViewController.modelArray = PlaygroundTeaserModel.createModelArray(itemJSONArray)
+//    }
+//}
 
 XCPShowView("NaviController", view: naviController.view)
 
